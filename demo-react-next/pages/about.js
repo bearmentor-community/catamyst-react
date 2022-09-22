16766e196d7d51744ccd01a4ec16fb5d93068c69
M Haidar Hanif <git@mhaidarhanif.com>
2022-09-23 01:01:50 +0700

    feat: add demo react with next.js

---
+++ demo-react-next/pages/about.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+
+      <main>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          illo, voluptatum voluptas eligendi magni hic, cum, eius rem at quidem
+          libero rerum atque accusamus neque deserunt aut nobis qui id.
+        </p>
+      </main>
+    </div>
+  );
+}
